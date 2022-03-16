d1e37ba642ae625e096b1a0a90903c4dfc93d6a1
maccam91 <michael.cortezjr@gmail.com>
2022-03-15 21:30:35 -0500

    added logo to homepage

--- src/Component/test.js
+++ src/Component/test.js
@@ -8,7 +8,7 @@ return(
     <div>
         <h1>Fire Side Gaming</h1>
         <h2>A place where you can showcase your content with your with friends</h2>
-        <img src='public/images/FiresideLogo.png'></img>
+        <img src='/images/FiresideLogo.png' alt='image'></img>
     </div>
 )
 }
